9e5860e48e780267edc71177fbe9b739fe0c1b05
Nariyaana Parnikova <quehacerestoy@gmail.com>
2023-10-29 16:28:27 +0300

    init commit

---
+++ webpack.common.js
@@ -0,0 +1,148 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin')
+
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['postcss-preset-env']]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        resourceQuery: /raw/,
+        type: 'asset/source'
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash][ext][query]'
+        }
+      },
+      {
+        test: /\.(ttf|otf)$/i,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]'
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].[contenthash].css'
+    }),
+
+    // Index
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+
+    //Section//
+    new HtmlWebpackPlugin({
+      template: './src/about.html',
+      filename: './about.html'
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/yourwave.html',
+      filename: './yourwave.html'
+    }),
+    // new HtmlWebpackPlugin({
+    //   template: './src/responsive-images.html',
+    //   filename: './responsive-images.html'
+    // }),
+    new HtmlWebpackPlugin({
+      template: './src/music.html',
+      filename: './music.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/events.html',
+      filename: './events.html'
+    }),
+
+    // Article
+    new HtmlWebpackPlugin({
+      template: './src/music/artists.html',
+      filename: './music/artists.html'
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/music/genres.html',
+      filename: './music/genres.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/yourwave/tests.html',
+      filename: './yourwave/tests.html'
+    }),
+
+    // Partials
+    new HtmlWebpackPartialsPlugin([
+      {
+        path: path.join(__dirname, './src/partials/analytics.html'),
+        location: 'analytics',
+        template_filename: '*',
+        priority: 'replace'
+      }
+    ])
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()]
+  }
+}
